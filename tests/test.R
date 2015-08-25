0331e3180993ac670f3a990db2f2133bbae4bc68
Thomas Rosendal <thomas.rosendal@sva.se>
2015-08-25 11:42:47 +0200

    bug in test

--- tests/test.R
+++ tests/test.R
@@ -2,8 +2,16 @@ library(svdc)
 
 stopifnot(identical(add(3,3), 6))
 
-tools::assertError(add("a","b"))
-tools::assertError(add(1,"b"))
-tools::assertError(add("a",1))
+res <- tools::assertError(add("a","b"))
+stopifnot(length(grep("Both arguments must be numeric",
+                      res[[1]]$message)) > 0)
+
+res <- tools::assertError(add(1,"b"))
+stopifnot(length(grep("Both arguments must be numeric",
+                      res[[1]]$message)) > 0)
+
+res <- tools::assertError(add("a",1))
+stopifnot(length(grep("Both arguments must be numeric",
+                      res[[1]]$message)) > 0)
 
 #Need to check if the errors are the correct erros too...
