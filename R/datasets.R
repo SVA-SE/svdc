b8d48febacded0744249533ccfa655eb098bed81
Thomas Rosendal <thomas.rosendal@sva.se>
2015-08-26 11:50:46 +0200

    Documentation for datasets

---
+++ R/datasets.R
@@ -0,0 +1,45 @@
+##' Postal codes
+##'
+##' @name postnummer
+##' @docType data
+##' @import sp
+##' @keywords datasets
+NULL
+
+##' Administrative (NUTS) units in Sweden (1 : 3 Million)
+##'
+##' The dataset contains administrative units (NUTS) in Sweden in
+##' European terminology in scale 1 : 3 Million.
+##'
+##' @name NUTS_03M
+##' @docType data
+##' @source
+##' \url{http://epp.eurostat.ec.europa.eu/portal/page/portal/gisco_Geographical_information_maps/popups/references/administrative_units_statistical_units_1}
+##' @import sp
+##' @keywords datasets
+##' @examples
+##'
+##' data(NUTS_03M)
+##' plot(NUTS_03M)
+##'
+NULL
+
+##' Veterinary districts spatial point dataframe
+##'
+##' The dataset contains point locations head offices of the veterinary districts in Sweden
+##'
+##' @name district_geo_RT90
+##' @docType data
+##' @source
+##' Swedish veterinary districts webpage
+NULL
+
+##' The name of the counties in Sweden (Län)
+##'
+##' A dataframe of names of counties, and the NUTS3 code
+##'
+##' @name nuts_label
+##' @docType data
+##' @source
+##' Extractions from Eurostat boundaries (\url{http://epp.eurostat.ec.europa.eu/portal/page/portal/gisco_Geographical_information_maps/popups/references/administrative_units_statistical_units_1})
+NULL
